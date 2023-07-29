4a9fc78b6cf08cc328706e054115a240ebeceb2e
Noodles <yilangcharleshe@gmail.com>
2023-07-29 01:25:25 -0400

    add login service

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,51 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  User,
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8DzPjd3SeRbDPneUU9gMKMaKhiyvXl7A",
+  authDomain: "yt-clone-4d696.firebaseapp.com",
+  projectId: "yt-clone-4d696",
+  appId: "1:404665185110:web:21bb5c9ac3db4cd3ff5cf7",
+  measurementId: "G-Z4SN2CBSVN",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWithGoogle() {
+  return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+  return auth.signOut();
+}
+
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(
+  callback: (user: User | null) => void
+) {
+  return onAuthStateChanged(auth, callback);
+}
---
+++ yt-web-client/app/navbar/sign-in.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Fragment } from "react";
+import styles from "./sign-in.module.css";
+import { signInWithGoogle, signOut } from "../firebase/firebase";
+import { User } from "firebase/auth";
+
+interface SignInProps {
+  user: User | null;
+}
+
+export default function SignIn({ user }: SignInProps) {
+  // determine the user state here
+  return (
+    <>
+      {user ? (
+        <button className={styles.signin} onClick={signOut}>
+          Sign Out
+        </button>
+      ) : (
+        <button className={styles.signin} onClick={signInWithGoogle}>
+          Sign In
+        </button>
+      )}
+    </>
+  );
+}
